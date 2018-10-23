5aab4eef4025a96cf0f5b6d6aa3826149d880091
Gabriel Genê <gabrielgene97@gmail.com>
2018-10-23 09:31:58 -0300

    Update topbar

--- src/pages/Register/topbar.jsx
+++ src/pages/Register/topbar.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import ArrowBack from '@material-ui/icons/ArrowBack';
+import CloseIcon from '@material-ui/icons/Close';
 
 const styles = {
   root: {
@@ -25,11 +24,8 @@ function Topbar(props) {
           <div className={classes.root}>
             <div onClick={() => props.goBack()} className={classes.back}>
               <IconButton color="inherit" aria-label="Menu">
-                <ArrowBack />
+                <CloseIcon />
               </IconButton>
-              <Typography variant="h6" color="inherit">
-                voltar
-              </Typography>
             </div>
           </div>
         </Toolbar>
